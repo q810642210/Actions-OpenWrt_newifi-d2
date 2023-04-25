6d7f98921596cdf7b0ac4f8500519c52ce801365
644234041 <123048795+644234041@users.noreply.github.com>
2023-04-25 19:00:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,10 @@
 #
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 # 修改固件名显示内核版本
 sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=k$(LINUX_VERSION)-/g' include/image.mk
 # 修改固件内核版本
-sed -i '/^KERNEL_PATCHVER:=/c\KERNEL_PATCHVER:=5.4' target/linux/x86/Makefile
+sed -i '/^KERNEL_PATCHVER:=/c\KERNEL_PATCHVER:=5.10' target/linux/x86/Makefile
 # 默认开启Wifi
 # sed -i 's/disabled=1/disabled=0/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
